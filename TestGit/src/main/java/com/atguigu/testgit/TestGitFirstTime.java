f3e931186672719c42faa71a678ad28359ac5eb2
张建春 <zhangjianchun8023@163.com>
2018-06-29 22:22:29 +0800

    这是用户1096的第一次提交！

--- TestGit/src/main/java/com/atguigu/testgit/TestGitFirstTime.java
+++ TestGit/src/main/java/com/atguigu/testgit/TestGitFirstTime.java
@@ -3,5 +3,6 @@ package com.atguigu.testgit;
 public class TestGitFirstTime {
 	public static void main(String[] args) {
 		System.out.println("这是第一个测试类！");
+		System.out.println("这是第一个测试类！hahaha");
 	}
 }

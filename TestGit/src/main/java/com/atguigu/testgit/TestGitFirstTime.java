fb041492f1bbaf23c6e955277dcc184a04ca1f8a
16619898420 <16619898420zjc@sina.com>
2018-06-29 22:33:36 +0800

    来自166的提交

--- TestGit/src/main/java/com/atguigu/testgit/TestGitFirstTime.java
+++ TestGit/src/main/java/com/atguigu/testgit/TestGitFirstTime.java
@@ -4,5 +4,6 @@ public class TestGitFirstTime {
 	public static void main(String[] args) {
 		System.out.println("这是第一个测试类！");
 		System.out.println("这是第一个测试类！hahaha");
+		System.out.println("这是第一个测试类！hahaha");
 	}
 }
